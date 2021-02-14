b30539fb5c826d98bfff68b25cd5f4719bb19767
Ryuya Ikeda <rikeda71@gmail.com>
2021-02-14 13:27:13 +0900

    convert js to tsx

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { NormalButton } from '../components/atoms/NormalButton';
+
+export default function Home() {
+  return <NormalButton text={'aaa'} />;
+}
